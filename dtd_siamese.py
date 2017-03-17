f866255a26176108567476bcd01f9f3f60b3576f
ospiro <ospiro@gmail.com>
2017-03-17 19:24:21 -0400

    rough draft bilinear term

--- dtd_siamese.py
+++ dtd_siamese.py
@@ -45,6 +45,7 @@ n_hidden_2 = 256 # 2nd layer number of features
 n_input = [28,28,3] # MNIST data input (img shape: 28*28)
 n_classes = 10 # MNIST total classes (0-9 digits)
 margin = 1.0
+bilinear_F_dim = 5
     
     
     # In[46]:
@@ -88,13 +89,21 @@ def tfNN(x):
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])#TODO:Change to smaller dim
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    W_fc2 = weight_variable([1024, 2])
+    F_bilinear_1 = weight_variable[1024,32]
+    W_bilinear_1 = weight_variable[1024,32]
+    b_bilinear_1 = bias_variable([32])
+    square_F = tf.matmul(F_bilinear_1,F_bilinear_1)
+    square = tf.matmul(h_fc1,tf.matmul(square,h_fc1)) 
+    linear = tf.matmul(h_fc1,W_bilinear_1)
+    bias = b_bilinear_1
+    h_bilinear_1 =  tf.nn.relu(square + linear + bias)
+    W_fc2 = weight_variable([32, 2])
     b_fc2 = bias_variable([2])
-    y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
+    y_conv = tf.matmul(h_bilinear_1, W_fc2) + b_fc2
     # layer_1 = tf.add(tf.matmul(x, weights['w1']), biases['b1'])
     # layer_1 = tf.nn.relu(layer_1)
     # layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
