8675a842410d8f30d6a1d9f4ad2e20d371c9796f
ospiro <ospiro@gmail.com>
2017-03-27 21:15:16 -0400

    PCA and baseline added

---
+++ PCA.py
@@ -0,0 +1,42 @@
+import sklearn
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.linear_model import SGDClassifier
+from scipy.misc import imread,imresize
+import random
+from os import listdir
+label_name_file = '/home/spiro/DTD_LABELS.txt'
+def get_label(im_fn):
+    with open(label_name_file,'r') as l:
+        label_names = l.read().splitlines()
+    label_dict = {}
+    for i, lab in enumerate(label_names):
+        label_dict.update({lab:i})
+    arr_fn = im_fn.split('_')
+    return label_dict[arr_fn[6]]
+files = listdir('/home/spiro/AlexNet/npz')
+K = 100
+features = np.zeros((len(files),256*K))
+labels = np.zeros(len(files))
+random.shuffle(files)
+for i,f in enumerate(files):
+    arr = np.load('/home/spiro/AlexNet/npz/'+f)
+    pca = PCA(n_components=K)
+    features[i,:] = np.reshape(pca.fit_transform(arr['arr_0']),(1,-1))
+    labels[i] =get_label('/home/spiro/AlexNet/npz/' + f)
+print features
+print labels
+print features.shape
+print labels.shape
+
+
+splitpoint = 4000
+valnum = features.shape[0]-splitpoint
+print "ASDF", valnum
+clf = SGDClassifier()
+#for i in range(features[:splitpoint].shape[0]):
+clf.partial_fit(features[:splitpoint],labels[:splitpoint],classes=np.unique(labels))
+preds = clf.predict(features[splitpoint:,:])
+np.save('fullpreds',preds)
+train_acc = np.sum(preds==labels[splitpoint:])/float(valnum)
+print train_acc
